fd21a5556a49cd2479cbb5c7d86a43a1d8de4d4d
Jin Hao <limjinhao@gmail.com>
2021-10-10 22:57:01 +0800

    Add Swagger to all services

--- backend/match/src/app.controller.ts
+++ backend/match/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
+@ApiTags('Server')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello()
+    return this.appService.getHello();
   }
 }
