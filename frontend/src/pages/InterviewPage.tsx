56af982058431ee8d06c7be6a91e190206d61590
ashley-lau <ashleylau451@gmail.com>
2021-10-10 16:05:32 +0800

    Clean up frontend slightly and set up simple routers

---
+++ frontend/src/pages/InterviewPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InterviewPage = () => {
+  return (
+    <div>
+      <h1>This page will contain the InterviewPage.</h1>
+    </div>
+  );
+};
+
+export default InterviewPage;
