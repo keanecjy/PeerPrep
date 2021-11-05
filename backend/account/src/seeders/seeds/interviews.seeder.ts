399ec23e3af61c80eadccc99470a5d79e3582700
Jin Hao <limjinhao@gmail.com>
2021-11-06 00:17:21 +0800

    Remove redundant data seeding

--- backend/account/src/seeders/seeds/interviews.seeder.ts
+++ backend/account/src/seeders/seeds/interviews.seeder.ts
@@ -13,6 +13,13 @@ export class InterviewsSeeder implements ISeeder {
   ) {}
 
   async seed(): Promise<any> {
+    if (
+      await this.interviewRepository.findOne({
+        owner: { id: '10c7e0a8-120b-45e0-a37f-be92170bfb8d' },
+      })
+    ) {
+      return;
+    }
     const interviews = this.interviewRepository.create({
       leetcodeSlug: 'two-sum',
       questionName: 'Two Sum',
